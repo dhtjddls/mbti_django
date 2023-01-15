04b9b7e7a333603d2a1eea3d92097efe897162c1
dhtjddls <dhtjddls12@naver.com>
2023-01-16 00:16:23 +0900

    feat: add /form & index pages

---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Question, Developer, Choice
+
+def index(request):
+    developers = Developer.objects.all()
+
+    context = {
+        'developers' : developers,
+    }
+    return render(request, 'index.html', context=context)
+
+def form(request):
+    questions = Question.objects.all()
+
+    context = {
+        'questions' : questions,
+    }
+    return render(request, 'form.html', context=context)
+
